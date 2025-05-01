81d5b8c735c6c7a704295811ef7d2959d75fe749
kol4egraf0-0 <genitya408@gmail.com>
2025-05-02 00:27:14 +0300

    fix bugs + add ai

--- backend/springdemo/email_extansion/content.js
+++ backend/springdemo/email_extansion/content.js
@@ -1,10 +1,25 @@
 console.log("Загрузочка произошла!!!")
 
-const observer = new MutationObserver((mutations)=>{ //изменения в браузере
-    for(const mutations of mutations){
-        const addedNodes = Array.from(mutations.addedNodes);
+function injectButton(){
+    const existingButton = document.querySelector('.ai-reply-button');
+}
+
+const observer = new MutationObserver((mutations)=>{ //изменения в браузере(api)
+    for(const mutation of mutations){
+        const addedNodes = Array.from(mutation.addedNodes);
         const hasComposeElements = addedNodes.some(node=>
-            node.nodeType === Node.ELEMENT_NODE
-        )
+            node.nodeType === Node.ELEMENT_NODE &&
+            (node.matches('.aDh, .btC, [role="dialog"]') 
+            || node.querySelector('.aDh, .btC, [role="dialog"]')) //элементы на странице гугла
+        );
+        if(hasComposeElements){
+            console.log("Окно для Отправки письма найдено")
+            setTimeout(injectButton, 500);
+        }
     }
 });
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+})
