6aeb5be63564a41141a90a52b01a6c29ba22816a
kol4egraf0-0 <genitya408@gmail.com>
2025-05-01 14:35:02 +0300

    add values + rW

--- backend/springdemo/src/main/java/com/ai/springdemo/app/EmailGeneratorService.java
+++ backend/springdemo/src/main/java/com/ai/springdemo/app/EmailGeneratorService.java
@@ -1,12 +1,28 @@
 package com.ai.springdemo.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.reactive.function.client.WebClient;
 import java.util.Map;
+
 
 @Service
 public class EmailGeneratorService {
-    
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build(); //не bean тк не настроен предвартиельно, сами задаем настройки
+    }
 
     public String generateEmailReply(EmailRequest emailRequest) {
         String prompt = buildPrompt(emailRequest);
@@ -17,7 +33,20 @@ public class EmailGeneratorService {
                                         Map.of("text", prompt)
                         })
                 });
+
+        //отдаем запрос и ожидаем ответа
+        String response = webClient.post()
+                .uri(geminiApiUrl+geminiApiKey)
+                .header("Content-Type", "application/json")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        //возращаем ответ с ai
+        return extractResponseContent(response);
     }
+
+    
 
     private String buildPrompt(EmailRequest emailRequest) {
         StringBuilder prompt = new StringBuilder();
