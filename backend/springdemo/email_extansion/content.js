9e7d622e50cb40aead246920a97fdccd863454f0
kol4egraf0-0 <genitya408@gmail.com>
2025-05-02 11:07:34 +0300

    update content.js

--- backend/springdemo/email_extansion/content.js
+++ backend/springdemo/email_extansion/content.js
@@ -1,7 +1,88 @@
-console.log("Загрузочка произошла!!!")
+function createAIButton() {
+    const button = document.createElement('div');
+    button.className = 'T-I J-J5-Ji aoO v7 T-I-atl L3'; //J-J5-Ji btA не эта тк оболочка
+    button.style.marginRight = '8px';
+    button.innerHTML = 'AI Reply';
+    button.setAttribute('role', 'button');
+    button.setAttribute('data-tooltip', 'Сгенерировать ИИ Ответ');
+    return button;
+}
+
+function getEmailContent() { //для отправки запроса берем текст!!!
+    const selectors =[ //перебераем div классы
+        '.h7',
+        '.a3s.aiL',
+        'gmail_quote',
+        '[role=presentation]'
+    ]
+    for(const selector of selectors){
+        const content = document.querySelector(selector);
+        if(content)
+        {
+            return content.innerText.trim(); //берем текст
+        }
+    }
+    return null;
+}
+
+
+function findComposeToolBar() {
+    const selectors =[ //перебераем div классы
+        '.btC',
+        '.aDh',
+        '[role=toolbar]',
+        '.gU.Up'
+    ]
+    for(const selector of selectors){
+        const toolbar = document.querySelector(selector);
+        if(toolbar)
+        {
+            return toolbar;
+        }
+    }
+    return null;
+}
 
 function injectButton(){
-    const existingButton = document.querySelector('.ai-reply-button');
+    const existingButton = document.querySelector('.ai-reply-button'); //внедрени кнопки в toolbar gmail'а
+    if(existingButton) existingButton.remove();
+
+    const toolbar = findComposeToolBar();
+    if(!toolbar){
+        console.log("Тулбар не нашлась")
+        return;
+    }
+
+    console.log("Тул нашлась, cоздаем кнопку");
+    const button = createAIButton();
+    button.classList.add('ai-reply-button');
+
+    button.addEventListener('Click', async ()=>{
+        try {
+            button.innerHTML='Generating';
+            button.disabled = 'true';
+
+            const emailContent = getEmailContent();
+            const response = await fetch('http://localhost:8080/api/email/generate',{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    emailContent:emailContent,
+                    "tone":"friendly"
+                })
+            });
+
+            if(!response.ok){
+                throw new Error('API Запрос провален!');
+            }
+        } catch (error) {
+            
+        }
+    });
+
+    toolbar.insertBefore(button, toolbar.firstChild);
 }
 
 const observer = new MutationObserver((mutations)=>{ //изменения в браузере(api)
